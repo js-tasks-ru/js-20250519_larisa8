6c158fa9696e77099f1d3a4117ce68bb1b8dc07b
Larisa Demidova <laro4ka.f@yandex.ru>
2025-05-27 14:26:24 +0300

    feat: add invertObj function

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,15 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+  if (!obj) {
+    return;
+  }
 
+  const inverted = {};
+
+  for (let [key, value] of Object.entries(obj)) {
+    inverted[value] = key;
+  }
+
+  return inverted;
 }
