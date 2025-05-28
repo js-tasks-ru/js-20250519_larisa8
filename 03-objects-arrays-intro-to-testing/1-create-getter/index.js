660d88da4d56f53d2d3085a856e3f50676de2be8
Larisa Demidova <laro4ka.f@yandex.ru>
2025-05-28 12:36:56 +0300

    feat: createGetter function

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,35 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const parts = path.split('.');
 
+  function getPropertyByPathIndex(obj, index) {
+    if (!obj) {
+      return;
+    }
+
+    const propertyName = parts[index];
+    const last = parts.length - 1;
+
+    if (index === last) {
+      return getOwnProperty(obj, propertyName);
+
+    } else {
+      index++;
+      obj = obj[propertyName];
+
+      return getPropertyByPathIndex(obj, index);
+    }
+  }
+
+  function getOwnProperty (obj, key) {
+    if (Object.hasOwn(obj, key)) {
+      return obj[key];  
+    }
+  }
+
+  return function(obj) {
+    let index = 0;
+    return getPropertyByPathIndex(obj, index);
+  };
 }
