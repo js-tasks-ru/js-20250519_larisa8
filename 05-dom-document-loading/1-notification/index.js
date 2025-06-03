5591e150b9e7747b8eb7c60df713f9e5f5834df2
Larisa Demidova <laro4ka.f@yandex.ru>
2025-06-03 15:59:12 +0300

    feat: add notification message component

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,71 @@
 export default class NotificationMessage {
+  static lastShown;
 
+  constructor(message, {
+    duration = 1000,
+    type = 'success'
+  } = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+    this.element = this.createElement(this.createTemplate());
+  }
+
+  getDurationStyleValue() {
+    return `${Math.floor(this.duration / 1000)}s`;
+  }
+
+  createTemplate() {
+    return `
+    <div class="notification ${this.type}" style="--value:${this.getDurationStyleValue()}">
+      <div class="timer"></div>
+      <div class="inner-wrapper">
+        <div class="notification-header">${this.type}</div>
+        <div class="notification-body">
+          ${this.message}
+        </div>
+      </div>
+    </div>
+    `;
+  }
+
+  createElement(template) {
+    const element = document.createElement('div');
+
+    element.innerHTML = template;
+    
+    return element.firstElementChild;
+  }
+
+  show(target) {
+    if (!target) {
+      target = document.body;
+    }
+
+    if (NotificationMessage.lastShown) {
+      NotificationMessage.lastShown.hide();
+    }
+    
+    NotificationMessage.lastShown = this;
+
+    target.append(this.element);
+    
+    this.timerId = setTimeout(() => { 
+      this.hide();
+    }, this.duration);
+  }
+
+  hide() {
+    this.destroy();
+  }
+
+  remove() {
+    this.element.remove();
+    NotificationMessage.lastShown = null;
+  }
+
+  destroy() {
+    this.remove();
+    clearTimeout(this.timerId);
+  }
 }
