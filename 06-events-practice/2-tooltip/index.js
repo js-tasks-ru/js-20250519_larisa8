0eb17ccbef6645546e286b63c929025a23b05942
Larisa Demidova <laro4ka.f@yandex.ru>
2025-06-06 15:37:05 +0300

    feat: add tooltip component

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,6 +1,71 @@
 class Tooltip {
-  initialize () {
+  static #instance;
 
+  constructor () {
+    if (Tooltip.#instance) {
+      return Tooltip.#instance;
+    }
+    Tooltip.#instance = this;
+    this.createListeners();
+  }
+
+  initialize () {
+    this.top = '';
+    this.left = '';
+    this.element = this.createElement(this.createTemplate());
+  }
+
+  render(text) {
+    this.element.textContent = text;
+    this.element.style.top = this.top;
+    this.element.style.left = this.left;
+    document.body.append(this.element);
+  }
+
+
+  createElement(template) {
+    const element = document.createElement('div');
+    element.innerHTML = template;
+    return element.firstElementChild;
+  }
+
+  createTemplate() {
+    return `
+      <div class="tooltip">${this.text}</div>
+    `;
+  }
+
+  handlePointerOver = (e) => {
+    const tooltip = e.target.dataset.tooltip;
+    
+    if (tooltip) {  
+      this.top = `calc(${e.y}px + 7px)`;
+      this.left = `calc(${e.x}px + 10px)`;
+      this.render(tooltip); 
+    }
+  } 
+
+  handlePointerOut = (e) => {
+    this.remove();
+  } 
+
+  createListeners() {
+    document.addEventListener('pointerover', this.handlePointerOver, true);
+    document.addEventListener('pointerout', this.handlePointerOut);
+  }
+
+  destroyListeners() {
+    document.removeEventListener('pointerover', this.handlePointerOver);
+    document.removeEventListener('pointerout', this.handlePointerOut);
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+    this.destroyListeners();
   }
 }
 
