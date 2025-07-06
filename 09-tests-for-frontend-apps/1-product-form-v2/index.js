e6f6fe2e635fe64727903f3a1f2c34246601dc34
Larisa Demidova <laro4ka.f@yandex.ru>
2025-07-06 16:07:24 +0300

    feat: product form v2 start work

--- 09-tests-for-frontend-apps/1-product-form-v2/index.js
+++ 09-tests-for-frontend-apps/1-product-form-v2/index.js
@@ -1,16 +1,14 @@
 import SortableList from '../2-sortable-list/index.js';
+import ProductFormV1 from '../../08-forms-fetch-api-part-2/1-product-form-v1/index.js';
 import escapeHtml from './utils/escape-html.js';
 import fetchJson from './utils/fetch-json.js';
 
 const IMGUR_CLIENT_ID = '28aaa2e823b03b1';
 const BACKEND_URL = 'https://course-js.javascript.ru';
 
-export default class ProductForm {
+export default class ProductForm extends ProductFormV1 {
   constructor (productId) {
+    super(productId);
     this.productId = productId;
-  }
-
-  async render () {
-
   }
 }
