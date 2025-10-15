d609f868ece8a0ea409e1ebbfad8a68131d291ec
Larisa Demidova <laro4ka.f@yandex.ru>
2025-10-15 16:18:22 +0300

    fix: delete unuse components

--- 11-building-project-with-webpack/components/ContentComponent.js
+++
@@ -1,7 +0,0 @@
-import BaseComponent from "./BaseComponent.js";
-
-export default class ContentComponent extends BaseComponent {
-  createTemplate() {
-    return `<div>${this.props.content}</div>`;
-  }
-}
